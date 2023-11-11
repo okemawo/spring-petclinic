fa0de8fa6d21827b2590cb7b582de69a1f6993a7
Net Coder <65502643+okemawo@users.noreply.github.com>
2023-11-11 04:19:30 -0500

    Update environmentInStage.groovy

--- environmentInStage.groovy
+++ environmentInStage.groovy
@@ -9,18 +9,18 @@ pipeline {
         sh 'mvn package -DskipTests'
       }
     }
-    stage('SonarQube analysis') {
-      steps {
-        withSonarQubeEnv('SonarQube') {
-          sh "sonar-scanner -X -Dsonar.host.url=http://192.168.82.165:9000 -Dsonar.login=squ_ababb5fa4a4d7ee9ec07b8b98a9300a0e3616f51"
-        }
-      }
-    }
     stage("Run") {
       steps {
         // Run the JAR file on port 8080
         // Replace 'target/my-app.jar' with the path to your JAR file
         sh 'nohup java -jar ./target/spring-petclinic-3.1.0-SNAPSHOT.jar --server.port=9090 > output.txt &'
+      }
+    }
+    stage('SonarQube analysis') {
+      steps {
+        withSonarQubeEnv('SonarQube') {
+          sh "sonar-scanner -X -Dsonar.host.url=http://192.168.82.165:9000 -Dsonar.login=squ_ababb5fa4a4d7ee9ec07b8b98a9300a0e3616f51"
+        }
       }
     }
   }

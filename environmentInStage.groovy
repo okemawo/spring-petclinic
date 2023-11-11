1fe14e7fa6e9ae6f00b44b9938a17cd89a81f839
Net Coder <65502643+okemawo@users.noreply.github.com>
2023-11-11 04:12:14 -0500

    Update environmentInStage.groovy

--- environmentInStage.groovy
+++ environmentInStage.groovy
@@ -2,18 +2,18 @@ pipeline {
   agent any
 
   stages {
-    stage('SonarQube analysis') {
-      steps {
-        withSonarQubeEnv('SonarQube') {
-          sh "sonar-scanner -X -Dsonar.host.url=http://192.168.82.165:9000 -Dsonar.login=squ_ababb5fa4a4d7ee9ec07b8b98a9300a0e3616f51"
-        }
-      }
-    }
     stage("Build") {
       steps {
         // Build the project with Maven
         sh 'mvn -version'
         sh 'mvn package -DskipTests'
+      }
+    }
+    stage('SonarQube analysis') {
+      steps {
+        withSonarQubeEnv('SonarQube') {
+          sh "sonar-scanner -X -Dsonar.host.url=http://192.168.82.165:9000 -Dsonar.login=squ_ababb5fa4a4d7ee9ec07b8b98a9300a0e3616f51"
+        }
       }
     }
     stage("Run") {

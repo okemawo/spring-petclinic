675ef67ea2ed313493db8516ad9d98234628fa7d
Net Coder <65502643+okemawo@users.noreply.github.com>
2023-11-15 03:28:16 -0500

    Update environmentInStage.groovy

--- environmentInStage.groovy
+++ environmentInStage.groovy
@@ -9,17 +9,17 @@ pipeline {
         sh 'mvn clean package'
       }
     }
-    stage("Run") {
-      steps {
-        // Run the JAR file on port 9090
-        sh 'nohup java -jar ./target/spring-petclinic-3.1.0-SNAPSHOT.jar --server.port=9090 > output.txt &'
-      }
-    }
     stage('SonarQube analysis') {
       steps {
         withSonarQubeEnv('SonarQube') {
           sh "sonar-scanner -X -Dsonar.host.url=${env.SONAR_URL} -Dsonar.login=${env.SONAR_TOKEN}"
         }
+      }
+    }
+    stage("Run") {
+      steps {
+        // Run the JAR file on port 9090
+        sh 'nohup java -jar ./target/spring-petclinic-3.1.0-SNAPSHOT.jar --server.port=9090 > output.txt &'
       }
     }
   }

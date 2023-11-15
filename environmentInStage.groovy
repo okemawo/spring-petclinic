b440cb045203e0fbdcc65170e5ae60b5f5d7a3e3
Net Coder <65502643+okemawo@users.noreply.github.com>
2023-11-15 06:12:30 -0500

    Update environmentInStage.groovy

--- environmentInStage.groovy
+++ environmentInStage.groovy
@@ -12,7 +12,7 @@ pipeline {
     stage("Run") {
       steps {
         // Run the JAR file on port 9090
-        sh 'nohup java -jar ./target/spring-petclinic-3.1.0-SNAPSHOT.jar --server.port=9090 > output.txt &'
+        sh 'nohup java -jar ./target/spring-petclinic-3.1.0-SNAPSHOT.jar --server.port=9095 > output.txt &'
       }
     }
   }

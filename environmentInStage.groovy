9b301348f3bf903b477bff05c51e75b1ae2a1cd6
Net Coder <65502643+okemawo@users.noreply.github.com>
2023-11-15 06:37:45 -0500

    Update environmentInStage.groovy

--- environmentInStage.groovy
+++ environmentInStage.groovy
@@ -7,6 +7,7 @@ pipeline {
         // Build the project with Maven
         sh 'mvn -version'
         sh 'mvn clean package -DskipTests'
+        sh 'ls'
         sh 'nohup java -jar ./target/spring-petclinic-3.1.0-SNAPSHOT.jar --server.port=9096 &'
       }
     }

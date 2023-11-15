d6799fbd8e50e3cdade4567c091b88d9f70927aa
Net Coder <65502643+okemawo@users.noreply.github.com>
2023-11-14 22:33:27 -0500

    Update environmentInStage.groovy

--- environmentInStage.groovy
+++ environmentInStage.groovy
@@ -6,7 +6,7 @@ pipeline {
       steps {
         // Build the project with Maven
         sh 'mvn -version'
-        sh 'mvn package -DskipTests'
+        sh 'mvn package'
       }
     }
     stage("Run") {

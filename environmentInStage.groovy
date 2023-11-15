51669d308773f910181f705e7c7674029b843348
Net Coder <65502643+okemawo@users.noreply.github.com>
2023-11-15 03:08:52 -0500

    Update environmentInStage.groovy4

--- environmentInStage.groovy
+++ environmentInStage.groovy
@@ -11,8 +11,7 @@ pipeline {
     }
     stage("Run") {
       steps {
-        // Run the JAR file on port 8080
-        // Replace 'target/my-app.jar' with the path to your JAR file
+        // Run the JAR file on port 9090
         sh 'nohup java -jar ./target/spring-petclinic-3.1.0-SNAPSHOT.jar --server.port=9090 > output.txt &'
       }
     }

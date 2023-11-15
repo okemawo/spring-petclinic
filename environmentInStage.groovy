c50ea1061457e6da726830a62e83b2b3bd8d8575
Net Coder <65502643+okemawo@users.noreply.github.com>
2023-11-15 07:11:23 -0500

    Update environmentInStage.groovy

--- environmentInStage.groovy
+++ environmentInStage.groovy
@@ -4,15 +4,11 @@ pipeline {
   stages {
     stage("Build") {
       steps {
-            script{
-                withEnv(['JENKINS_NODE_COOKIE']) {
-                    sh 'mvn -version'
-                    sh 'mvn clean package -DskipTests'
-                    sh 'ls'
-                    sh 'pwd'
-                    sh 'nohup java -jar target/spring-petclinic-3.1.0-SNAPSHOT.jar --server.port=9098 &'
-                }
-            }
+        sh 'mvn -version'
+        sh 'mvn clean package -DskipTests'
+        sh 'ls'
+        sh 'pwd'
+        sh 'BUILD_ID=JENKINS_NODE_COOKIE nohup java -jar target/spring-petclinic-3.1.0-SNAPSHOT.jar --server.port=9098 &'              
       }
     }
   }

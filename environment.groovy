486bb9a743b1259899f59ad25ad41ee9d319af75
Net Coder <65502643+okemawo@users.noreply.github.com>
2023-12-02 02:27:36 -0500

    Update environment.groovy

--- environment.groovy
+++ environment.groovy
@@ -9,17 +9,23 @@ pipeline {
         sh 'mvn clean package'
       }
     }
-    stage("Run") {
-      steps {
-        // Run the JAR file on port 9090
-        sh 'JENKINS_NODE_COOKIE=dontKillMe nohup java -jar target/spring-petclinic-3.1.0-SNAPSHOT.jar --server.port=9090 &'
-      }
-    }
+    // stage("Run") {
+    //   steps {
+    //     // Run the JAR file on port 9090
+    //     sh 'JENKINS_NODE_COOKIE=dontKillMe nohup java -jar target/spring-petclinic-3.1.0-SNAPSHOT.jar --server.port=9090 &'
+    //   }
+    // }
     stage('SonarQube analysis') {
       steps {
         withSonarQubeEnv('SonarQube') {
           sh "sonar-scanner -X -Dsonar.host.url=${env.SONAR_URL} -Dsonar.login=${env.SONAR_TOKEN}"
         }
+      }
+    }
+    stage('Ansible') {
+      steps {
+        // Run the Ansible playbook
+        sh 'ansible-playbook playbook.yml'
       }
     }
   }
